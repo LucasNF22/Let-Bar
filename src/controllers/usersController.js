83fe20e6ee004b72279435822985234d205ee111
LucasNF22 <lucasnfiorentino@gmail.com>
2022-05-12 17:33:24 -0300

    Se agregaron validaciones en formulario de register y carga de imagenes con multer

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,73 +1,74 @@
 const path = require("path")
 const fs = require("fs");
-const multer = require("multer");
-const res = require("express/lib/response");
-const bcrypt= require ('bcryptjs');
-const {validationResult}=require("express-validator");
-
-
-
+const bcrypt = require('bcryptjs');
+const { validationResult } = require("express-validator");
 
 
 const usersControllers = {
-    registro: (req, res) => {         
-        res.render (path.join(__dirname, "../views/register"))
-     },
-
-    procesarRegistro: (req, res)=>{
+    
+    //Carga el register 
+    registro: (req, res) => {
+        res.render(path.join(__dirname, "../views/register"))
+    },
+    //Procesa el register
+    procesarRegistro: (req, res) => {
         const usuariosFilePath = path.join(__dirname, '../data/usersDataBase.json');
         const usuarios = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
+
+        const validaciones = validationResult(req);
         
         console.log("---------------------");
         console.log("llego el envio")
-        //console.log(uploadFile);
+        console.log(req.file);
+        console.log(validaciones.mapped());
         console.log("---------------------");
-        //console.log(usuarios)
-        let usuariosOriginales = usuarios;
+        
+        
+        
 
+        if(validaciones.errors.length > 0){
+                return res.render(path.join(__dirname, "../views/register"), { 
+                errors: validaciones.mapped(),
+                oldData: req.body 
+            }); 
+        
+        }
+        
+        
+        let usuariosOriginales = usuarios;
         let ultimoObjeto = (usuariosOriginales.length) - 1;
         let ultimoId = usuariosOriginales[ultimoObjeto].id;
         let nuevoId = ultimoId + 1;
         let passEncriptada = bcrypt.hashSync(req.body.password, 10);
-console.log(passEncriptada);
+
 
         let nuevoUsuario = {
             "id": nuevoId,
-            "first_name": req.body.name,
-            "last_name": " ",
+            "first_name": req.body.first_name,
+            "last_name": req.body.last_name,
             "email": req.body.email,
             "password": passEncriptada,
             "tel": req.body.tel,
-            "avatar": " ",
+            "avatar": req.file.filename,
             "category": "user",
-            "direccion": "direccion",
-            "localidad":"localidad",
-            "codigo_postal":"codigo_postal",
-            "fecha_nac":req.body.birthday,
-            
-            
-
+            "birthday": req.body.birthday,
+            "direccion": " ",
+            "localidad": " ",
+            "codigo_postal": " "
         };
 
-
-        
-        //console.log(nuevoUsuario);
-        
+        //Actualiza el array con el nuevo usuario
         usuariosOriginales.push(nuevoUsuario);
         let usuariosActualizados = usuariosOriginales;
-        
-       fs.writeFileSync(usuariosFilePath, JSON.stringify(usuariosActualizados, null, ' '));
+
+        //Guarda los usuarios actualizados
+        fs.writeFileSync(usuariosFilePath, JSON.stringify(usuariosActualizados, null, ' '));
         res.redirect("/Home")
-
-
-        console.log("---------------------");
-       // console.log(req.body);
-        console.log("---------------------");
-        
+    
     },
 
     login: (req, res) => {
-        res.render (path.join (__dirname, "../views/login"))
+        res.render(path.join(__dirname, "../views/login"))
     },
 
     control: (req, res) => {
@@ -75,44 +76,43 @@ console.log(passEncriptada);
     },
 
     //Funcion procesar login
+    procesarLogin: (req, res) => {
 
-    procesarLogin:(req, res) => {
-        
-        let errors =validationResult(req);
+        let errors = validationResult(req);
         let usuarioALoguearse;
-        if (errors.isEmpty()){
+        if (errors.isEmpty()) {
             const usuariosFilePath = path.join(__dirname, '../data/usersDataBase.json');
             const users = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8'));
 
-           // let usersJSON= fs.readFileSync('usersDataBase.json',( users.json, {encoding: utf8}));
-            
-         //let users;
-          /*  if(usersJSON == ""){
+            // let usersJSON= fs.readFileSync('usersDataBase.json',( users.json, {encoding: utf8}));
 
-                users=[];
-            } else{
-                users=JSON.parse(userJSON);
-            }*/
-            for (let i = 0; i< users.length; i++) {
-                if(users[i].email==req.body.email) {
-                    if (bcrypt.compareSync(req.body.password, users[i].password)){
-                    let usuarioALoguearse =users[i];
-                    break;
+            //let users;
+            /*  if(usersJSON == ""){
+  
+                  users=[];
+              } else{
+                  users=JSON.parse(userJSON);
+              }*/
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].email == req.body.email) {
+                    if (bcrypt.compareSync(req.body.password, users[i].password)) {
+                        let usuarioALoguearse = users[i];
+                        break;
 
-                 }
+                    }
                 }
 
             }
-        if (usuarioALoguearse == undefined){
-             res.redirect('/users/login', {errors: [{msg:"Credenciales invalidas"}]}
-            );
-        }
-        req.session.usuarioLogueado = usuarioALoguearse;
-        res.redirect ('succes');
+            if (usuarioALoguearse == undefined) {
+                res.redirect('/users/login', { errors: [{ msg: "Credenciales invalidas" }] }
+                );
+            }
+            req.session.usuarioLogueado = usuarioALoguearse;
+            res.redirect('succes');
 
 
-        } else{
-             res.redirect('/users/login',{errors:errors.errors});
+        } else {
+            res.redirect('/users/login', { errors: errors.errors });
         }
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const usersControllers = require ("../controllers/usersController");
+const usersControllers = require("../controllers/usersController");
 const multer = require('multer');
 const path = require('path');
+var router = express.Router()
+
 //const session= require('express-session');
-const req = require("express/lib/request");
-const {check}= require("express-validator");
+
+const { check } = require("express-validator");
+
+const { body } = require('express-validator');
+
+const Logger = require("nodemon/lib/utils/log");
 
 
 /* Configuracion donde se guardan las imagenes con MULTER */
@@ -14,45 +20,152 @@ const storage = multer.diskStorage({
         cb(null, carpetaDeGuardado);
     },
     filename: (req, file, cb) => {
-        
         let nombreImagen = Date.now() + path.extname(file.originalname);
         cb(null, nombreImagen);
     }
 });
- 
+
 /* Middleware de multer */
-const uploadFile = multer({ storage });
+const uploadFile = multer({ storage: storage }) /*, 
+    limits:{fileSize: 1000000}, 
+    fileFiler: function(req, file, cb){
+        let extensionesValidas = ['.jpg', '.png', '.gif'];
+             if (file){
+                let fileExtension = path.extname(file.originalname);
+                if (!extensionesValidas.includes(fileExtension)) {
+                    new Error(`Las extensiones de imagen permitidas son: ${extensionesValidas.join(', ')}`);
+                }
+            }
+        }*/
+   
 
 
-var router = express.Router()
+// validaciones para register
+const validacionesRegister = [
+    body("first_name")
+        .notEmpty().withMessage("Debes introducir tu nombre").bail()
+        .isLength({ min: 4 }).withMessage("tu nombre debe tener mínimo 4 letras")
+        .isAlpha().withMessage("solo puede introducir letras"),
 
-router.get ("/register", usersControllers.registro);
-router.post("/register", uploadFile.single('avatar'), usersControllers.procesarRegistro);
+    body("last_name")
+        .notEmpty().withMessage("Debes introducir tu apellido").bail()
+        .isLength({ min: 4 }).withMessage("tu apellido debe tener mínimo 4 letras")
+        .isAlpha().withMessage("solo puede introducir letras"),
 
-router.get ("/login", usersControllers.login);
+    body("email")
+        .notEmpty().withMessage("Debes introducir un correo").bail()
+        .isEmail().withMessage("Debes introducir un correo válido"),
+
+    body("password")
+        //.trim() //Elimina espacio inicial y final
+        .notEmpty().withMessage("Debes introducir una contraseña")
+        .isLength({ min: 6 }).withMessage('La contraseña debe contener minimo 8 caracteres')  // validacion minimo y maximo de caracteres
+        .isAlphanumeric().withMessage("La contraseña debe contener al menos un número y una letra"),   //validacion alfanumerica 
+
+    body("pass_confirm")
+        .custom(async (pass_confirm, { req }) => {
+            const password = req.body.password
+            if (password !== pass_confirm) {
+                throw new Error('Las contraseñas deben coincidir') //validacion custom para que coincidan las contraseñas
+            }
+            return true
+        }),
+
+    body("tel")
+        .notEmpty().withMessage("Debes introducir un numero telefónico")
+        .isNumeric().withMessage("Solo puede ingresar caracteres numericos"),
+
+    body("birthday")
+        .notEmpty().withMessage("Debes introducir tu fecha de nacimiento")
+        .custom((date, { req }) => {
+            let birthday = req.body.birthday;
+
+            function calcularEdad(birthday) {
+                var hoy = new Date();
+                var cumpleanos = new Date(birthday);
+                var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+                var m = hoy.getMonth() - cumpleanos.getMonth();
+                if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+                    edad--;
+                }
+                return edad;
+            }
+            var edad = calcularEdad(birthday);
+
+            if (edad < 18) {
+                throw new Error("Debes ser mayor de 18 años")
+            }
+            return true
+        }),
+
+    body('avatar')
+        .custom((value, { req }) => {
+            let file = req.file;
+            let extensionesValidas = ['.jpg', '.png', '.gif'];
+
+            if (!file) {
+                throw new Error('Tenes que seleccionar una imagen');
+
+            } else if (file) {
+                let fileExtension = path.extname(file.originalname);
+                if (!extensionesValidas.includes(fileExtension)) {
+                    throw new Error(`Las extensiones de imagen permitidas son: ${extensionesValidas.join(', ')}`);
+                }
+
+            }
+            return true;   
+        })
+        .custom((value, { req }) => { 
+            let file= req.file
+            if (file) {
+                let tamaño = file.size
+                    if (parseInt(tamaño) > 1000000) {
+                    throw new Error('El tamaño de imagen maximo permitido es de 1MB');
+                }
+            }
+                return true;
+            
+
+            
+        } )         /* ,check("*").escape()*/
+
+
+]
+
+// Rutas registro
+
+router.get("/register", usersControllers.registro);
+router.post("/register/process", uploadFile.single('avatar'), validacionesRegister, usersControllers.procesarRegistro,);
+
+
+// Rutas Login
+router.get("/login", usersControllers.login);
 
 /* Validación de datos ingresados */
 
 /*Middleware errores */
 
-let errores= [
+let errores = [
     check('email').isEmail().withMessage("El correo no es valido"),
-    check ('password'). isLength({min: 2}).withMessage('La contraseña debe tener al menos 2 caracteres'),
- ];
- 
-router.post("/login",errores, usersControllers.procesarLogin);
+    check('password').isLength({ min: 2 }).withMessage('La contraseña debe tener al menos 2 caracteres'),
+];
+
+router.post("/login", errores, usersControllers.procesarLogin);
 
 
 
-router.get('/check', function(req, res){
-    if (req.session.usuarioLogueado == undefined){
+router.get('/check', function (req, res) {
+    if (req.session.usuarioLogueado == undefined) {
         res.send("No estas Logueado");
 
-    }else {
+    } else {
         res.send("El usuario logueado es" + req.session.usuarioLogueado.email);
     }
 })
 
-router.get ("/panel-control", usersControllers.control);
+
+// Rutas Panel de control
+
+router.get("/panel-control", usersControllers.control);
 
 module.exports = router
