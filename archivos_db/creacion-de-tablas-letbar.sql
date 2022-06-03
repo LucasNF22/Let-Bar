42a056e9123a1431a99c51ce9adf3275730adc61
LucasNF22 <lucasnfiorentino@gmail.com>
2022-06-02 22:04:06 -0300

    se modifico el ancho de la tarjeta popUp de productos. Se crearon las tablas de la base de datos. Se añade script de la creacion de las tablas y archivo de la DB. Se añade diagramas de la base de datos y funcionamiento de la compra actualizados

---
+++ archivos_db/creacion-de-tablas-letbar.sql
@@ -0,0 +1,98 @@
+create table users (
+id INT primary key auto_increment not null,
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+email varchar(50) not null,
+password varchar(100) not null,
+tel int not null,
+avatar varchar(100) not null,
+birthday date not null
+);	
+
+create table users_categories (
+id INT primary key auto_increment not null,
+user_type varchar(20) not null
+);	
+
+alter table users 
+add category_id int not null;
+
+create table addresses (
+id INT primary key auto_increment not null,
+province varchar(20) not null,
+locality varchar(20) not null,
+street varchar(50) not null,
+street_number int not null,
+comments varchar(200) not null,
+user_id int not null,
+address_alias varchar(20) not null
+);	
+
+create table users_payments(
+id INT primary key auto_increment not null,
+payment_method_id varchar(20) not null,
+user_id varchar(20) not null
+);
+
+create table payment_methods(
+id INT primary key auto_increment not null,
+name varchar(20) not null
+);
+
+create table payment_methods_data(
+id INT primary key auto_increment not null,
+card_number int(16),
+card_bank varchar(20),
+owner varchar(50),
+cbu int(50),
+mp_alias varchar(50),
+mp_cvu int(50),
+payment_id int(16)
+);
+
+create table shopping_carts(
+id INT primary key auto_increment not null,
+user_id int(50) not null,
+product_id int(20) not null,
+status varchar(50) not null,
+payment_method_id int(20) not null
+);
+
+create table products (
+id INT primary key auto_increment not null,
+name varchar(50) not null,
+description varchar(200) not null,
+image varchar(50) not null,
+category_id int(20) not null,
+brand varchar(20) not null,
+size varchar(100) not null,
+priceUnit decimal not null,
+cantDisc int(20), 
+priceCant decimal,
+offer boolean not null,
+year int,
+stock int not null,
+graduation decimal,  
+cantValoration decimal, 
+acuValoration int,
+valoration decimal
+);	
+
+
+create table product_categories(
+id INT primary key auto_increment not null,
+category varchar(50) not null,
+name varchar(50) not null,
+icon varchar(50) not null
+);
+
+
+
+
+
+
+
+
+
+
+
---
+++ archivos_db/letbar-db.sql
@@ -0,0 +1,278 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: letbar-db
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `addresses`
+--
+
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `province` varchar(20) NOT NULL,
+  `locality` varchar(20) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `street_number` int(11) NOT NULL,
+  `comments` varchar(200) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address_alias` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `addresses`
+--
+
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment_methods`
+--
+
+DROP TABLE IF EXISTS `payment_methods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment_methods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment_methods`
+--
+
+LOCK TABLES `payment_methods` WRITE;
+/*!40000 ALTER TABLE `payment_methods` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment_methods` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment_methods_data`
+--
+
+DROP TABLE IF EXISTS `payment_methods_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment_methods_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_number` int(16) DEFAULT NULL,
+  `card_bank` varchar(20) DEFAULT NULL,
+  `owner` varchar(50) DEFAULT NULL,
+  `cbu` int(50) DEFAULT NULL,
+  `mp_alias` varchar(50) DEFAULT NULL,
+  `mp_cvu` int(50) DEFAULT NULL,
+  `payment_id` int(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment_methods_data`
+--
+
+LOCK TABLES `payment_methods_data` WRITE;
+/*!40000 ALTER TABLE `payment_methods_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment_methods_data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_categories`
+--
+
+DROP TABLE IF EXISTS `product_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `icon` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_categories`
+--
+
+LOCK TABLES `product_categories` WRITE;
+/*!40000 ALTER TABLE `product_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `category_id` int(20) NOT NULL,
+  `brand` varchar(20) NOT NULL,
+  `size` varchar(100) NOT NULL,
+  `priceUnit` decimal(10,0) NOT NULL,
+  `cantDisc` int(20) DEFAULT NULL,
+  `priceCant` decimal(10,0) DEFAULT NULL,
+  `offer` tinyint(1) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `graduation` decimal(10,0) DEFAULT NULL,
+  `cantValoration` decimal(10,0) DEFAULT NULL,
+  `acuValoration` int(11) DEFAULT NULL,
+  `valoration` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shopping_carts`
+--
+
+DROP TABLE IF EXISTS `shopping_carts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shopping_carts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(50) NOT NULL,
+  `product_id` int(20) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `payment_method_id` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopping_carts`
+--
+
+LOCK TABLES `shopping_carts` WRITE;
+/*!40000 ALTER TABLE `shopping_carts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shopping_carts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `tel` int(11) NOT NULL,
+  `avatar` varchar(100) NOT NULL,
+  `birthday` date NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_categories`
+--
+
+DROP TABLE IF EXISTS `users_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_categories`
+--
+
+LOCK TABLES `users_categories` WRITE;
+/*!40000 ALTER TABLE `users_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_payments`
+--
+
+DROP TABLE IF EXISTS `users_payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_payments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `payment_method_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_payments`
+--
+
+LOCK TABLES `users_payments` WRITE;
+/*!40000 ALTER TABLE `users_payments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'letbar-db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-02 21:57:37
