05d8546ec6953be6f24a472f404109559174a3f1
AnaLauCas <anacasadomoran@gmail.com>
2022-06-13 22:41:32 -0300

    Crud en proceso

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -219,45 +219,4 @@ const usersControllers = {
     
 }
 
-{
-    let usersControllers = {
-        registro: function (req, res){
-            db.users.findAll()
-            .then (function(users){
-                return res.render ("listadoDeUsuarios", {users : users})
-            })
-
-             },
-
-        procesarRegistro: function (req,res){
-            db.users.create({
-                first_name:req.body.first_name,
-                last_name: req.body.last_name ,
-                email: req.body.email ,
-                password: req.body.password ,
-                tel: req.body.tel ,
-                birthday: req.body.birthday ,
-                avatar: req.file.avatar,
-
-            })
-            res.redirect ("/users")
-        },
-        
-        listadoUsuarios: function (req, res){
-            db.users.findAll ()
-            .then (function (users){
-                res.render ("listadoDeUsuarios", {users : users})
-            })
-        },
-
-        perfil: function (req, res){
-            db.perfil.findByPK (req.params.id)
-            .then (function (users) {
-                res.render ("perfil", {users : users})
-            })
-        }
-    }
-
-}
-
 module.exports = usersControllers;
