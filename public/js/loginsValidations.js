c2c92245b00a10fe1951775d4393581cf84e20f0
LucasNF22 <lucasnfiorentino@gmail.com>
2022-06-30 18:53:01 -0300

    Se completo las validacions del front para el login

--- public/js/loginsValidations.js
+++ public/js/loginsValidations.js
@@ -2,15 +2,15 @@ window.onload = function () {
 
     // Se capturan las variables -------------
     let inputEmail = document.getElementById("email");
-    let inputPasswords = document.getElementById('password');
-    
+    let inputPassword = document.getElementById('password');
+
 
     // Se captura el formulario
-    let form = document.querySelector(".FormCarga");
+    let form = document.getElementById("form");
 
     // Se capturan los Ul de errores -------------
-    let nameErrors = document.getElementById("emailErrors");
-    let brandErrors = document.getElementById('passwordsErrors');
+    let emailErrors = document.getElementById("emailErrors");
+    let passwordErrors = document.getElementById('passwordErrors');
 
     form.addEventListener("submit", (e) => {
 
@@ -20,45 +20,43 @@ window.onload = function () {
         let emailErrorsAcu = 0;
         let passwordErrorsAcu = 0;
 
-         // Se definen las validaciones -------------
+        // Se definen las validaciones -------------
 
         // Validaciones de Email
         if (inputEmail.value === "") {
+            inputEmail.classList.add('control-error')
             emailErrors.innerHTML = "";
             emailErrors.innerHTML += '<li class="feedback">' + "Debes introducir un email" + '</li>';
             emailErrorsAcu++;
         } else {
+            inputEmail.classList.remove('control-error')
             emailErrors.innerHTML = "";
             emailErrorsAcu = 0;
         }
 
-        // Validaciones de Passwords
-        if (inputPasswordsvalue === "") {
-            passwordsErrors.innerHTML = "";
-            passwordsErrors.innerHTML += '<li class="feedback">' + "Debes introducir una contraseña" + '</li>';
-            passwordsErrorsAcu++;
-        } else if (inputBrand.value.length < 3) {
-            passwordsErrors.innerHTML = ""
-            passwordsErrorsAcu++;
-            brandErrors.innerHTML += '<li class="feedback">' + "introducir una contraseña mas larga" + '</li>'
+        // Validaciones de Password
+        if (inputPassword.value === "") {
+            inputPassword.classList.add('control-error');
+            passwordErrors.innerHTML = "";
+            passwordErrors.innerHTML += '<li class="feedback">' + "Debes introducir una contraseña" + '</li>';
+            passwordErrorsAcu++;
         } else {
-            passwordsErrors.innerHTML = "";
-            passwordsErrorsAcu = 0
+            inputPassword.classList.remove('control-error')
+            passwordErrors.innerHTML = "";
+            passwordErrorsAcu = 0;
         }
 
-         // Se suman los acumuladores de errores ------------------
-         let AcuErrors =
-         emailErrorsAcu +
-         passwordsErrorsAcu +
-
-         console.log(inputImagen.value);
+        // Se suman los acumuladores de errores ------------------
+        let AcuErrors =
+            emailErrorsAcu +
+            passwordErrorsAcu;
 
 
-         // Si no hay errores se hace el submit del formulario
-         if (AcuErrors === 0) {
-             form.submit();
-         }
- 
-     })
- 
- }
+        // Si no hay errores se hace el submit del formulario
+        if (AcuErrors === 0) {
+            form.submit();
+        }
+
+    })
+
+}
